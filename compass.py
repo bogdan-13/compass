e78b81050f8ef4666ce3c4100be61a3fdcdb839a
Bogdan Kalnybolotchuk <bogdan.kalnybolotchuk@gmail.com>
2022-01-02 04:52:28 +0200

    add exceptions handler

--- compass.py
+++ compass.py
@@ -1,6 +1,17 @@
 def direction(facing: str, turn: int) -> str:
     directions = ('N', 'NE', 'E', 'SE', 'S', 'SW', 'W', 'NW')
-    azimuth_steps = turn // 45
-    start = directions.index(facing)
-    direction_index = (start + azimuth_steps) % len(directions)
-    return directions[direction_index]
+    try:
+        if not isinstance(facing, str) or not isinstance(turn, int):
+            raise TypeError
+
+        if (turn % 45 != 0) or (facing not in directions) or abs(turn) > 1080:
+            raise ValueError
+
+        azimuth_steps = turn // 45
+        start = directions.index(facing)
+        direction_index = (start + azimuth_steps) % len(directions)
+        return directions[direction_index]
+    except TypeError:
+        print('Invalid argument data type')
+    except ValueError:
+        print('Incorrect argument values')
