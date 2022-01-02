2937c568038cf539c87d22343928ca0205f01de9
Bogdan Kalnybolotchuk <bogdan.kalnybolotchuk@gmail.com>
2022-01-02 03:50:53 +0200

    add base solution

--- compass.py
+++ compass.py
@@ -1,3 +1,6 @@
-def direction(facing, turn):
-    # your smart code here
-    return "S"
+def direction(facing: str, turn: int) -> str:
+    directions = ('N', 'NE', 'E', 'SE', 'S', 'SW', 'W', 'NW')
+    azimuth_steps = turn // 45
+    start = directions.index(facing)
+    direction_index = (start + azimuth_steps) % len(directions)
+    return directions[direction_index]
